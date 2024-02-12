f454ab9e4f4b990c655a5d293f4c43dec1776c4a
btadikon <btadikon@uncc.edu>
2024-02-11 23:31:14 -0500

    Completed Instructions

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,20 +1,21 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js/auto';
+import * as d3 from 'd3';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'pb-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss',
-  host: {ngSkipHydration : 'true'}
+  styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements AfterViewInit{
+export class HomepageComponent implements AfterViewInit {
 
-  public dataSource = {
+  public dataSource: any = {
     datasets: [
-        {
-            data: [] as any[],
-            backgroundColor: [
+      {
+        data: [],
+        backgroundColor: [
                 'red',
                 '#ffcd56',
                 '#ff6384',
@@ -22,21 +23,85 @@ export class HomepageComponent implements AfterViewInit{
                 '#fd6b19',
                 '#0ac360',
                 '#4d5791',
-            ]
-        }
+        ],
+      }
     ],
-    labels: [] as any[]
+
+    labels: []
   };
 
+  public dataSource1: any = [];
+
+  public svg: any;
+  public width = 750;
+  public height = 350;
+  public radius = Math.min(this.width, this.height) / 2;
+  public colors: any;
+
+  public createSvg(): void {
+    this.svg = d3.select("#pie-chart")
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.width / 2 + "," + this.height / 2 + ")"
+      );
+  }
+  public createColors(): void {
+    this.colors = d3.scaleOrdinal()
+      .domain(this.dataService.getDataSource1().map((d: any) => d.value))
+      .range([
+            'red',
+            '#ffcd56',
+            '#ff6384',
+            '#36a2eb',
+            '#fd6b19',
+            '#0ac360',
+            '#4d5791',
+      ]);
+  }
+
+  private drawChart(): void {
+    const pie = d3.pie<any>().value((d: any) => Number(d.value));
+    this.svg
+      .selectAll('pieces')
+      .data(pie(this.dataService.getDataSource1()))
+      .enter()
+      .append('path')
+      .attr('d', d3.arc()
+        .innerRadius(0)
+        .outerRadius(this.radius)
+      )
+      .attr('fill', (d: any, i: any) => (this.colors(i)))
+      .attr("stroke", "#121926")
+      .style("stroke-width", "1px");
+
+    const labelLocation = d3.arc()
+      .innerRadius(100)
+      .outerRadius(this.radius);
+
+    this.svg
+      .selectAll('pieces')
+      .data(pie(this.dataService.getDataSource1()))
+      .enter()
+      .append('text')
+      .text((d: any) => d.data.label)
+      .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
+      .style("text-anchor", "middle")
+      .style("font-size", 15);
+  }
+
+  /*
   @ViewChild('myChart')
   eleRef!: ElementRef;
+  */
+  constructor(private http: HttpClient, public dataService: DataService) {
 
-    constructor(private http: HttpClient, private chartRef: ElementRef){
-
-    }
-
-
-    ngAfterViewInit(): void {
+  }
+  /*
+  ngAfterViewInit(): void {
       this.http.get('http://localhost:3000/budget')
       .subscribe((res: any) => {
         for(var i = 0; i < res.myBudget.length; i++){
@@ -47,13 +112,48 @@ export class HomepageComponent implements AfterViewInit{
       this.createChart();
 
       });
-    }
+  }
+  */
+  ngAfterViewInit(): void {
+    if (
+      this.dataService.getDataSource().datasets[0].data.length == 0 ||
+      this.dataService.getDataSource1().length == 0
+    ) {
+      this.dataService.fetchDataFromBackend().subscribe((res: any) => {
+        for (var i = 0; i < res.myBudget.length; i++) {
+          this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+          this.dataSource.labels[i] = res.myBudget[i].title;
 
-    createChart(){
-      var ctx = this.chartRef.nativeElement.querySelector('#myChart').getContext('2d');
-      var myPieChart = new Chart(ctx, {
-          type: "pie",
-          data: this.dataSource
+          this.dataSource1.push({
+            "label": res.myBudget[i].title,
+            "value": res.myBudget[i].budget,
+          });
+        }
+        this.dataService.setDataSource(this.dataSource);
+        this.dataService.setDataSource1(this.dataSource1);
+
+        this.createChart();
+        this.createSvg();
+        this.createColors();
+        this.drawChart();
       });
     }
+    else {
+      this.createChart();
+      this.createSvg();
+      this.createColors();
+      this.drawChart();
+
+    }
   }
+
+
+  createChart() {
+    var ctx = <HTMLCanvasElement>document.getElementById("myChart");
+    var myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: this.dataService.getDataSource()
+    });
+  }
+}
+
